985f30476d79897510fae5f5ed424559e81c4995
EmanoelV <dev.emanoel@gmail.com>
2024-02-03 09:26:13 -0300

    add global styles

--- lib/text_plus.dart
+++ lib/text_plus.dart
@@ -6,7 +6,37 @@ import 'package:text_plus/utils/format_text.dart';
 
 class TextPlus extends StatelessWidget {
   final String text;
-  const TextPlus(this.text, {super.key});
+  final Locale? locale;
+  final bool? softWrap;
+  final TextOverflow? overflow;
+  final int? maxLines;
+  final String? semanticsLabel;
+  final TextWidthBasis? textWidthBasis;
+  final TextAlign? textAlign;
+  final TextDirection? textDirection;
+  final Color? selectionColor;
+  final StrutStyle? strutStyle;
+  final TextHeightBehavior? textHeightBehavior;
+  final TextStyle? style;
+  final TextScaler? textScaler;
+
+  const TextPlus(
+    this.text, {
+    super.key,
+    this.locale,
+    this.softWrap,
+    this.overflow,
+    this.maxLines,
+    this.semanticsLabel,
+    this.textWidthBasis,
+    this.textAlign,
+    this.textDirection,
+    this.selectionColor,
+    this.strutStyle,
+    this.textHeightBehavior,
+    this.style,
+    this.textScaler,
+  });
 
   /// build text with formatted text
   List<InlineSpan> _buildText(String text) {
@@ -28,6 +58,22 @@ class TextPlus extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) =>
-      Text.rich(TextSpan(children: _buildText(text)));
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(children: _buildText(text)),
+      locale: locale,
+      softWrap: softWrap,
+      overflow: overflow,
+      maxLines: maxLines,
+      semanticsLabel: semanticsLabel,
+      textWidthBasis: textWidthBasis,
+      textAlign: textAlign,
+      textDirection: textDirection,
+      selectionColor: selectionColor,
+      strutStyle: strutStyle,
+      textHeightBehavior: textHeightBehavior,
+      style: style,
+      textScaler: textScaler,
+    );
+  }
 }
